34f219a5e53c3f14773112f565e9732ba91ebe08
roma <roma-lysyk@ukr.net>
2026-01-05 12:19:57 +0200

    updated dependencies, added locale layouts, and included a new cloud icon

---
+++ app/[locale]/cart/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
+
+import { Language } from '@/models/language';
+import { generateCatalogMetadata } from '@/utils/metadata';
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: Language }> }): Promise<Metadata> {
+	const { locale } = await params;
+	return generateCatalogMetadata({ locale, urlPath: `cart` });
+}
+
+export default async function LocaleLayout({ children }: { children: ReactNode }) {
+	return (
+		<>
+			{ children }
+		</>
+	);
+};
